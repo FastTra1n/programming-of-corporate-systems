9718434e0cc285e2f372af9efed0e11e2edaafc0
critical_error <->
2025-10-17 16:42:59 +0300

    feat: added last two tasks from workbook 2.

---
+++ workbook_2/task_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LabExperiment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void doExperiment(int bacteria, int mul)
+            {
+                int antibiotic = 10;
+                int hour = 0;
+
+                while (bacteria > 0 && antibiotic > 0)
+                {
+                    bacteria *= 2;
+                    bacteria -= antibiotic * mul;
+                    antibiotic -= 1;
+                    hour += 1;
+                    Console.WriteLine($"После {hour} часа бактерий осталось: {bacteria}");
+                }
+            }
+
+            int n, m;
+
+            Console.Write("Введите количество бактерий: ");
+            n = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Введите количество антибиотика: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            doExperiment(n, m);
+        }
+    }
+}
---
+++ workbook_2/task_7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MarsColonization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool canPlace(int quanity, int width, int height, int max_width, int max_height, int thickness)
+            {
+                int final_width = width + 2 * thickness;
+                int final_height = height + 2 * thickness;
+                int total_quanity = max_width / final_width * max_height / final_height;
+                return total_quanity >= quanity;
+            }
+
+            int n, a, b, w, h;
+
+            Console.Write("Введите n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите w: ");
+            w = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите h: ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            int left = 0;
+            int right = Math.Max(w, h);
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (canPlace(n, a, b, w, h , mid))
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            Console.WriteLine($"Ответ: d = {left}");
+        }
+    }
+}
