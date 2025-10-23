b4e7405ddc01e8167ae0d13552e97cc727f2afcb
critical_error <->
2025-10-23 20:30:16 +0300

    fix(workbook2): added exception handling for task 2.

--- workbook_2/task_2/Program.cs
+++ workbook_2/task_2/Program.cs
@@ -10,14 +10,30 @@ namespace LuckyTicket
                 uint number;
                 ushort firstPart, secondPart;
                 byte firstSum, secondSum;
-
-                Console.Write("Введите номер билета: ");
-                number = Convert.ToUInt32(Console.ReadLine());
-                firstPart = (ushort)(number / 1000);
-                secondPart = (ushort)(number % 1000);
-                firstSum = (byte)(firstPart / 100 + firstPart / 10 % 10+ firstPart % 10);
-                secondSum = (byte)(secondPart / 100 + secondPart / 10 % 10 + secondPart % 10);
-                Console.WriteLine(firstSum == secondSum);
+                
+                try
+                {
+                    Console.Write("Введите номер билета: ");
+                    number = Convert.ToUInt32(Console.ReadLine());
+                    if (number > 999999 || number < 100000)
+                    {
+                        Console.WriteLine("Число содержит более/менее 6 знаков. Введите целое 6-значное число.");
+                        continue;
+                    }
+                    firstPart = (ushort)(number / 1000);
+                    secondPart = (ushort)(number % 1000);
+                    firstSum = (byte)(firstPart / 100 + firstPart / 10 % 10 + firstPart % 10);
+                    secondSum = (byte)(secondPart / 100 + secondPart / 10 % 10 + secondPart % 10);
+                    Console.WriteLine(firstSum == secondSum);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Недопустимый ввод для номера билета. Проверьте корректность введённого числа.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Число выходит за рамки допустимого диапазона чисел. Введите целое, неотрицательное 6-тизначное число.");
+                }
             } while (true);
         }
     }
