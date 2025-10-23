a7249f54a13939fba9a1fd3b3c250abd756e9ede
critical_error <->
2025-10-23 21:19:36 +0300

    fix(workbook2): added exception handling for task 7.

--- workbook_2/task_7/Program.cs
+++ workbook_2/task_7/Program.cs
@@ -6,42 +6,54 @@ namespace MarsColonization
     {
         static void Main(string[] args)
         {
-            bool canPlace(int quanity, int width, int height, int max_width, int max_height, int thickness)
+            bool canPlace(uint quanity, uint width, uint height, uint max_width, uint max_height, uint thickness)
             {
-                int final_width = width + 2 * thickness;
-                int final_height = height + 2 * thickness;
-                int total_quanity = max_width / final_width * max_height / final_height;
-                return total_quanity >= quanity;
+                uint final_width = width + 2 * thickness;
+                uint final_height = height + 2 * thickness;
+                uint total_quanity_h = max_width / final_width * max_height / final_height;
+                uint total_quanity_v = max_width / final_height * max_height / final_width;
+                return (Math.Max(total_quanity_h, total_quanity_v) >= quanity);
             }
 
-            int n, a, b, w, h;
+            uint n, a, b, w, h;
 
-            Console.Write("Введите n: ");
-            n = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите a: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите b: ");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите w: ");
-            w = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите h: ");
-            h = Convert.ToInt32(Console.ReadLine());
-
-            int left = 0;
-            int right = Math.Max(w, h);
-            while (left < right)
+            try
             {
-                int mid = (left + right) / 2;
-                if (canPlace(n, a, b, w, h , mid))
+                Console.Write("Введите n: ");
+                n = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Введите a: ");
+                a = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Введите b: ");
+                b = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Введите w: ");
+                w = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Введите h: ");
+                h = Convert.ToUInt32(Console.ReadLine());
+
+                uint left = 0;
+                uint right = Math.Max(w, h);
+                while (left < right)
                 {
-                    left = mid;
+                    uint mid = (left + right + 1) / 2;
+                    if (canPlace(n, a, b, w, h, mid))
+                    {
+                        left = mid;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
                 }
-                else
-                {
-                    right = mid - 1;
-                }
+                Console.WriteLine($"Ответ: d = {left}");
             }
-            Console.WriteLine($"Ответ: d = {left}");
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный ввод. Введите целое, неотрицательное число.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Слишком большое число. Введите число поменьше.");
+            }
         }
     }
 }
