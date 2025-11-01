31ba79115afde727d4c8f1acde32c520395fc6d8
critical_error <->
2025-11-01 15:31:51 +0300

    fix(workbook2): fixed minor bugs in LuckyTicket.

--- workbook_2/task_2/Program.cs
+++ workbook_2/task_2/Program.cs
@@ -15,18 +15,47 @@ namespace LuckyTicket
                 try
                 {
                     Console.Write("Введите номер билета: ");
-                    userInput = Console.ReadLine();
+                    userInput = Console.ReadLine().Trim();
                     number = Convert.ToUInt32(userInput);
-                    if (userInput.Length != 6 || number > 999999 || number < 100000)
+                    if (userInput.Length > 1 && userInput[0] == '0')
                     {
-                        Console.WriteLine("Число содержит более/менее 6 знаков. Введите целое 6-значное число.");
-                        continue;
+                        if (userInput.Length != 6)
+                        {
+                            Console.WriteLine("Число содержит более или менее 6 знаков. Введите целое 6-значное число.");
+                            continue;
+                        }
+
+                        firstPart = (ushort)(number / 1000);
+                        secondPart = (ushort)(number % 1000);
+                        firstSum = (byte)(firstPart / 100 + firstPart / 10 % 10 + firstPart % 10);
+                        secondSum = (byte)(secondPart / 100 + secondPart / 10 % 10 + secondPart % 10);
+                        Console.WriteLine(firstSum == secondSum);
                     }
-                    firstPart = (ushort)(number / 1000);
-                    secondPart = (ushort)(number % 1000);
-                    firstSum = (byte)(firstPart / 100 + firstPart / 10 % 10 + firstPart % 10);
-                    secondSum = (byte)(secondPart / 100 + secondPart / 10 % 10 + secondPart % 10);
-                    Console.WriteLine(firstSum == secondSum);
+                    else
+                    {
+                        if (userInput[0] == '+')
+                        {
+                            Console.WriteLine("Введите 6-значное целое число без указания знака числа.");
+                            continue;
+                        }
+                        if (number > 999999)
+                        {
+                            Console.WriteLine("Число содержит более 6 знаков. Введите целое 6-значное число.");
+                            continue;
+                        }
+
+                        if (number < 100000)
+                        {
+                            Console.WriteLine("Число содержит менее 6 знаков. Введите целое 6-значное число.");
+                            continue;
+                        }
+
+                        firstPart = (ushort)(number / 1000);
+                        secondPart = (ushort)(number % 1000);
+                        firstSum = (byte)(firstPart / 100 + firstPart / 10 % 10 + firstPart % 10);
+                        secondSum = (byte)(secondPart / 100 + secondPart / 10 % 10 + secondPart % 10);
+                        Console.WriteLine(firstSum == secondSum);
+                    }
                 }
                 catch (FormatException)
                 {
